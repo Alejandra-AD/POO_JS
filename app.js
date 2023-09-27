f701d37933b924a2b113eacf3abc38326f2facba
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-09-26 22:23:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ function Persona(nombre){
     this.saludar = function(){
         return `${this.nombre} dice hola 👋`;
     }
+    // this.saludarIngles = function(){
+    //     return `${this.nombre} says hi 👋`;
+    // }
 
+}
+// crear un prototipo para no pasar ciertos métodos a todas las instancias que creemos
+
+Persona.prototype.saludarIngles = function(){
+    return `${this.nombre} says hi 👋`;
 }
 
 const juanito = new Persona ("Juanito");
